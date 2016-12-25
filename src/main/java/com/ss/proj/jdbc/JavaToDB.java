67ee5986abd9c7a26d52da0a7c89277e0a155673
sreeni padarthi <sreeni padarthi>
2016-12-25 19:01:29 +0100

    JR-20145 update dbconnect class

--- src/main/java/com/ss/proj/jdbc/JavaToDB.java
+++ src/main/java/com/ss/proj/jdbc/JavaToDB.java
@@ -1,6 +1,12 @@
 package com.ss.proj.jdbc;
 
+import com.ss.proj.jdbc.exmpl.MyJDBC;
+
 public class JavaToDB {
 
-	
+	public static void main(String[] args) {
+		MyJDBC jdbc = new MyJDBC();
+		jdbc.getStudentData();
+		System.out.println("Done");
+	}
 }
